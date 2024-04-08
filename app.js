3112005fe5d4b877786812683016c2ae12c8c4e0
LEE <tpqms775@naver.com>
2024-04-08 15:12:20 +0900

    feat: 이벤트 발생시 기본동작이 제한하여 submit 클릭시 새로고침이 안되게 한다.

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = loginForm.querySelector('input');
-const loginButton = loginForm.querySelector('button');
 
-function onLoginSubmit() {
+function onLoginSubmit(event) {
+
     const input = loginInput.value;
+    event.preventDefault();
     console.log(loginInput.value);
 
 }
-loginButton.addEventListener('click', onLoginSubmit);
+
+loginForm.addEventListener('submit', onLoginSubmit);
+// submit 이벤트는 기본적으로 새로고침을 발생시킨다. 
+// 이를 막기 위해 preventDefault()를 사용한다.
+// submit 이벤트는 form 태그에서만 발생한다.
+// 엔터또는 버튼을 누르면 submit 이벤트가 발생한다.
