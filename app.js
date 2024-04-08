05c3f77a975ac164a300ff18e75c452288f34e1f
LEE <tpqms775@naver.com>
2024-04-08 15:56:09 +0900

    feat: 유저정보 존재 여부에 따라 다르게 보여준다.

--- app.js
+++ app.js
@@ -3,18 +3,28 @@ const loginInput = loginForm.querySelector('input');
 const greeting = document.querySelector('#greeting');
 
 const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME = 'username';
 
 function onLoginSubmit(event) {
     event.preventDefault();
     const input = loginInput.value;
-    localStorage.setItem('username', input);
+    localStorage.setItem(USERNAME, input);
     loginForm.classList.add(HIDDEN_CLASSNAME);
     greeting.innerText = `Hello ${input}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener('submit', onLoginSubmit);
 // submit 이벤트는 기본적으로 새로고침을 발생시킨다. 
 // 이를 막기 위해 preventDefault()를 사용한다.
 // submit 이벤트는 form 태그에서만 발생한다.
 // 엔터또는 버튼을 누르면 submit 이벤트가 발생한다.
+
+const savedUsername = localStorage.getItem(USERNAME);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener('submit', onLoginSubmit);
+}else{
+    greeting.innerText = `Hello ${savedUsername}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
