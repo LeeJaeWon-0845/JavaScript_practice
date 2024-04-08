34e210a247e54652604de5631a7dd1ed38ba72ba
LEE <tpqms775@naver.com>
2024-04-08 14:41:49 +0900

    refactor: 자바스크립트가 아닌 HTML에서 유효성 검사하기

--- app.js
+++ app.js
@@ -4,12 +4,7 @@ const loginButton = loginForm.querySelector('button');
 
 function onLoginSubmit() {
     const input = loginInput.value;
-    if (input === '') {
-        alert('Please write your name');
-    } else if (input.length > 15) {
-        alert('Your name is too long');
-    } else {
-        console.log('Hello', loginInput.value);
-    }
+    console.log(loginInput.value);
+
 }
 loginButton.addEventListener('click', onLoginSubmit);
