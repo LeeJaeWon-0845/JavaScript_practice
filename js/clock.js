35afbf2b8140b597a2b30052c1331b2b49c3134d
LEE <tpqms775@naver.com>
2024-04-08 20:48:07 +0900

    feat: 1초가 아닌 01초로 나오게 문자열 포맷팅

--- js/clock.js
+++ js/clock.js
@@ -3,9 +3,9 @@ const clock = document.querySelector('#clock');
 function getClock(){
     const date = new Date();
     
-    const h = date.getHours();
-    const m = date.getMinutes();
-    const s = date.getSeconds();
+    const h = date.getHours().toString().padStart(2, "0");
+    const m = date.getMinutes().toString().padStart(2, "0");
+    const s = date.getSeconds().toString().padStart(2, "0");
     clock.innerHTML = `${h}:${m}:${s}`;
     //console.log(`${h}:${m}:${s}`);
 }
